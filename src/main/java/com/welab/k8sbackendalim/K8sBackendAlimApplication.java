e9c3062163ae7d4ce78d5595f6fbe3cd2d68b478
Sunhokim2 <mcwright@naver.com>
2025-06-12 15:32:44 +0900

    버전0.0.2 카프카 컨슈머

--- src/main/java/com/welab/k8sbackendalim/K8sBackendAlimApplication.java
+++ src/main/java/com/welab/k8sbackendalim/K8sBackendAlimApplication.java
@@ -2,7 +2,10 @@ package com.welab.k8sbackendalim;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+// 요청할때만 넣는다. 0611 기준 알림에서 요청을 다른 서비스로 보내는건 없으니 필요없는데 그냥 넣어놈 ㅎ
+@EnableFeignClients
 @SpringBootApplication
 public class K8sBackendAlimApplication {
 
